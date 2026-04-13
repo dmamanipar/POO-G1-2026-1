7400133d7ddce1ea3aaef8d5162e07111a82ea65
dmamanipar <dmamanipar@unsa.edu.pe>
2026-04-13 09:40:58 -0500

    gui p2

--- SysVentVehiculo/src/main/java/pe/edu/upeu/App.java
+++ SysVentVehiculo/src/main/java/pe/edu/upeu/App.java
@@ -1,39 +1,21 @@
 package pe.edu.upeu;
 
-import pe.edu.upeu.enums.TipoVehiculo;
-import pe.edu.upeu.model.Cliente;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-import java.util.ArrayList;
-import java.util.List;
-
-/**
- * Hello world!
- *
- */
-public class App {
+public class App extends Application {
     public static void main( String[] args ) {
-        System.out.println(TipoVehiculo.SEDAN.toString());
+        launch(args);
+    }
 
-        for (TipoVehiculo tv:TipoVehiculo.values()){
-            System.out.println(tv.toString());
-        }
-
-        List<Cliente> clientes=new ArrayList<>();
-        clientes.add(new Cliente("01", "David Mamani",
-                "951782520", "c01@gmail.com"));
-
-        clientes.add(new Cliente("02", "Raul Gomez Apaza",
-                "951782521", "c02@gmail.com"));
-
-        clientes.add(new Cliente("03", "Pedro Gonzales Torres",
-                "951782522", "c03@gmail.com"));
-
-        clientes.add(new Cliente("04", "Alex Raul Peredo Paredes",
-                "951782523", "c04@gmail.com"));
-
-        for (Cliente c: clientes){
-            System.out.println(c.toString());
-        }
-
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader=new FXMLLoader(getClass()
+                .getResource("/view/maingui.fxml"));
+        Scene scene=new Scene(loader.load(), 400, 600);
+        stage.setScene(scene);
+        stage.show();
     }
 }
