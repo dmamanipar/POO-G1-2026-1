f486c24273ccd351bde5a51fb311eea61c3d1b67
dmamanipar <dmamanipar@unsa.edu.pe>
2026-05-05 12:56:01 -0500

    Ejemplo Micronaut Repository

--- SysVentasVehiculo/src/main/java/pe/edu/upeu/model/Cliente.java
+++ SysVentasVehiculo/src/main/java/pe/edu/upeu/model/Cliente.java
@@ -1,6 +1,30 @@
 package pe.edu.upeu.model;
 
-public class Cliente {
+import com.ctc.wstx.shaded.msv.relaxng_datatype.Datatype;
+import io.micronaut.data.annotation.Id;
+import io.micronaut.data.annotation.MappedEntity;
+import io.micronaut.data.annotation.MappedProperty;
+import io.micronaut.data.model.DataType;
+import lombok.*;
 
+@MappedEntity(value = "cliente")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Cliente {
+    @Id
+    @MappedProperty(value = "dni", type = DataType.STRING)
+    private String dni;
+
+    @MappedProperty(value = "nombre", type = DataType.STRING)
+    private String nombre;
+
+    @MappedProperty(value = "telefono", type = DataType.STRING)
+    private String telefono;
+
+    @MappedProperty(value = "email", type = DataType.STRING)
+    private String email;
 
 }
