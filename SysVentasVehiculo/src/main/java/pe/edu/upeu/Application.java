1c4d010c7b926811fa5752e05f216f3a12729bdc
alexpehu <mamanipari@gmail.com>
2026-05-18 09:02:45 -0500

    Proyecto nuevo

--- SysVentasVehiculo/src/main/java/pe/edu/upeu/Application.java
+++
@@ -1,8 +0,0 @@
-package pe.edu.upeu;
-public class Application {
-
-    public static void main(String[] args) {
-        //Micronaut.run(Application.class, args);
-        JavaFxApp.launch(JavaFxApp.class, args);
-    }
-}
